6fd64c6f02c939829d367e120bc33f6d33d39339
ElMat1 <ElMat1@github.com>
2022-04-22 12:33:31 -0400

    Creacion del modelo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@
 
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  devise_for :usuarios, controllers: {
+    sessions: 'usuarios/sessions',
+    registrations: 'usuarios/registrations'
+    },
+    path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout', sing_up: 'register'
+    }
+
 end
