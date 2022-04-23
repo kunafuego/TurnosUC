8cabf5c908499d8642da1c1fe948df64efa5a6fc
Domingo Agüero <domingo.aguero@uc.cl>
2022-04-23 19:44:46 -0400

    CRUD de turnos listo

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -5,7 +5,8 @@ class TurnosController < ApplicationController
 
   def create
     @turnos_params = params.require(:turno).permit(:tipo, :limite_personas, :direccion_llegada, 
-      :dia_de_la_semana, :direccion_salida, :hora_salida, :id_creador)
+      :dia_de_la_semana, :direccion_salida, :hora_salida)
+    @turnos_params[:id_creador] = current_usuario.id
     @turno = Turno.create(@turnos_params)
     if @turno.save
       redirect_to turnos_index_path, notice: 'Turno Creado'
@@ -13,13 +14,39 @@ class TurnosController < ApplicationController
       redirect_to turnos_index_path, notice: 'Turno no Creado'
     end
   end
+
   def index
-    @turnos = Turno.all
+    @mis_turnos = Turno.where(:id_creador => current_usuario.id).all
+    @turnos_buscador = Turno.all - @mis_turnos
   end
 
   def show
+    @turno = Turno.find(params[:id])
+    @creador = Usuario.find(params[:id_creador])
+    #Con esta info debo buscar en las otras tablas los elementos que quiero mandar al show
   end
 
   def edit
+    @turno = Turno.find(params[:id])
   end
+
+  def update
+    @turno = Turno.find(params[:id])
+    @turnos_new_params = params.require(:turno).permit(:tipo, :limite_personas, :direccion_llegada, 
+      :dia_de_la_semana, :direccion_salida, :hora_salida, :id_creador)
+    if @turno.update(@turnos_new_params)
+      redirect_to turnos_index_path, notice: 'Turno editado'
+    else
+      redirect_to turnos_index_path, notice: 'Turno no editado'
+    end
+  end
+
+  def delete
+    @turno = Turno.find(params[:id])
+    @turno.destroy
+
+    redirect_to turnos_index_path, notice: 'Turno eliminado'
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  
+  ########################## TURNOS ############################
   get 'turnos/new'
   post 'turnos', to: 'turnos#create'
   get 'turnos/index'
   get 'turnos/show'
   get 'turnos/edit'
-  root to: 'pages#home'
+  patch 'turnos/update', to: 'turnos#update', as: 'turnos_update'
+  delete 'turnos/delete', to: 'turnos#delete', as: 'turnos_delete'
+
+
 
   devise_for :usuarios, controllers: {
     sessions: 'usuarios/sessions',
