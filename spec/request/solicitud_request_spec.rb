e641f0be6a46574790c71391442920b78865e16d
ElMat1 <ElMat1@github.com>
2022-05-04 17:49:00 -0400

    Fix Tests Solicitudes

--- spec/request/solicitud_request_spec.rb
+++ spec/request/solicitud_request_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe Solicitud, type: :request do
+
+  before(:each) do
+    # Aqui se utiliza la factory de usuarios, para crear uno
+    @usuario = create(:usuario)
+    # Usamos el metodo definido en support
+    sign_in @usuario
+
+    # Aqui se utiliza la factory de turnos, para crear uno
+    @turno = create(:turno)
+
+    # Aqui creamos la solicitud
+    @solicitud = Solicitud.create!( { id_usuario: @usuario.id, id_turno: @turno.id, descripcion: "si si"} )
+  
+  end
+
+  # Empiezan los test
   describe 'get index' do
     it 'deberia retornar una respuesta existosa' do
-      get '/solicituds'
+      get '/solicituds/index'
       expect(response).to have_http_status(:ok)
     end
   end
@@ -31,7 +47,7 @@ RSpec.describe Solicitud, type: :request do
 
   describe 'edit' do
     it 'deberia retornar una respuesta existosa' do
-      get "/solicituds/#{solicitud.id}/edit"
+      get solicituds_edit_path(id: @solicitud.id)
       expect(response).to have_http_status(:ok)
     end
   end
@@ -40,23 +56,23 @@ RSpec.describe Solicitud, type: :request do
     it 'deberia actualizar la solicitud' do
       expect do
         patch solicituds_update_path(id: @solicitud.id), params: { solicitud: { descripcion: 'hola hola' } }
-        solicitud.reload
-      end.to change(solicitud, :descripcion)
+        @solicitud.reload
+      end.to change(@solicitud, :descripcion)
     end
   end
   describe 'update' do
     it 'no deberia actualizar la solicitud' do
       expect do
         patch solicituds_update_path(id: @solicitud.id), params: { solicitud: { descripcion: 'mas de 100 caracteres                                                                                ' } }
-        solicitud.reload
-      end.to_not change(solicitud, :descripcion)
+        @solicitud.reload
+      end.to_not change(@solicitud, :descripcion)
     end
   end
 
   describe 'delete' do
     it 'deberia disminuir en 1 la cantidad de solicitudes' do
       expect do
-        delete "/solicituds/#{solicitud.id}"
+        delete solicituds_delete_path(id: @solicitud.id)
       end.to change(Solicitud, :count).by(-1)
     end
   end
