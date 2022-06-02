984c6dd29e26ac1b20bf87a6e1893bf7e7ff9573
ElMat1 <ElMat1@github.com>
2022-06-01 23:01:42 -0400

    Search Bar

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -9,22 +9,11 @@ class TurnosController < ApplicationController
 
   def create
     @params = params.require(:turno).permit(:tipo, :limite_personas, :dia_de_la_semana,
-                                                   :calle_salida, :numero_salida, :comuna_salida,
-                                                        :calle_llegada, :numero_llegada, :comuna_llegada, 
-                                                            :hora, :minutos)
-    @params[:hora_salida] = "#{@params[:hora]}:#{@params[:minutos]}"
-    @params.delete(:hora)
-    @params.delete(:minutos)
+                                            :calle_salida, :numero_salida, :comuna_salida,
+                                            :calle_llegada, :numero_llegada, :comuna_llegada,
+                                            :hora, :minutos)
 
-    @params[:direccion_salida] = "#{@params[:calle_salida]} #{@params[:numero_salida]}, #{@params[:comuna_salida]}"
-    @params.delete(:calle_salida)
-    @params.delete(:numero_salida)
-    @params.delete(:comuna_salida)
-
-    @params[:direccion_llegada] = "#{@params[:calle_llegada]} #{@params[:numero_llegada]}, #{@params[:comuna_llegada]}"
-    @params.delete(:calle_llegada)
-    @params.delete(:numero_llegada)
-    @params.delete(:comuna_llegada)
+    @params = manejar_parametros(@params)
     if usuario_signed_in?
       @params[:id_creador] = current_usuario.id
       @turno = Turno.create(@params)
@@ -39,23 +28,27 @@ class TurnosController < ApplicationController
   end
 
   def index
-    params[:razon] = 'Buscador de Turnos' unless params.key?(:razon)
-    @mis_turnos = if current_usuario
-                    Turno.where(id_creador: current_usuario.id).all
-                  else
-                    []
-                  end
-    if current_usuario
-      @pertenezco = PerteneceA.where(id_usuario: current_usuario.id).all
+    if params[:busqueda_por_direccion]
+      search_term = params[:busqueda_por_direccion].downcase
+      @turnos = Turno.all.select do |turno|
+        turno.direccion_llegada.downcase.include?(search_term) ||
+          turno.direccion_salida.downcase.include?(search_term)
+      end
+    else
+      @turnos = Turno.all
+    end
+
+    case params[:razon]
+    when 'Mis Turnos'
+      @turnos = usuario_signed_in? ? @turnos.where(id_creador: current_usuario.id) : []
+    when 'Turnos de los que Participo'
+      @pertenezco = usuario_signed_in? ? PerteneceA.where(id_usuario: current_usuario.id) : []
       @ids_turnos_que_pertenezco = []
       @pertenezco.each do |turno|
         @ids_turnos_que_pertenezco << turno.id_turno
       end
-      @turnos_que_estoy = Turno.where(id: @ids_turnos_que_pertenezco).all
-    else
-      @turnos_que_estoy = []
+      @turnos = @turnos.where(id: @ids_turnos_que_pertenezco)
     end
-    @turnos_buscador = Turno.all
   end
 
   def show
@@ -75,22 +68,11 @@ class TurnosController < ApplicationController
 
   def update
     @new_params = params.require(:turno).permit(:tipo, :limite_personas, :dia_de_la_semana,
-                                              :calle_salida, :numero_salida, :comuna_salida,
-                                                  :calle_llegada, :numero_llegada, :comuna_llegada, 
-                                                      :hora, :minutos)
-    @new_params[:hora_salida] = "#{@new_params[:hora]}:#{@new_params[:minutos]}"
-    @new_params.delete(:hora)
-    @new_params.delete(:minutos)
+                                                :calle_salida, :numero_salida, :comuna_salida,
+                                                :calle_llegada, :numero_llegada, :comuna_llegada,
+                                                :hora, :minutos)
 
-    @new_params[:direccion_salida] = "#{@new_params[:calle_salida]},#{@new_params[:numero_salida]},#{@new_params[:comuna_salida]}"
-    @new_params.delete(:calle_salida)
-    @new_params.delete(:numero_salida)
-    @new_params.delete(:comuna_salida)
-
-    @new_params[:direccion_llegada] = "#{@new_params[:calle_llegada]},#{@new_params[:numero_llegada]},#{@new_params[:comuna_llegada]}"
-    @new_params.delete(:calle_llegada)
-    @new_params.delete(:numero_llegada)
-    @new_params.delete(:comuna_llegada)
+    @new_params = manejar_parametros(@new_params)
     if @turno.update(@new_params)
       redirect_to turnos_index_path, notice: 'Turno editado'
     else
--- app/helpers/turnos_helper.rb
+++ app/helpers/turnos_helper.rb
@@ -12,4 +12,22 @@ module TurnosHelper
   def mostrar_salirse_del_turno(idt, idu)
     PerteneceA.exists?(id_usuario: idu, id_turno: idt)
   end
+
+  def manejar_parametros(params)
+    params[:hora_salida] = "#{params[:hora]}:#{params[:minutos]}"
+    params.delete(:hora)
+    params.delete(:minutos)
+
+    params[:direccion_salida] = "#{params[:calle_salida]} #{params[:numero_salida]}, #{params[:comuna_salida]}"
+    params.delete(:calle_salida)
+    params.delete(:numero_salida)
+    params.delete(:comuna_salida)
+
+    params[:direccion_llegada] = "#{@params[:calle_llegada]} #{params[:numero_llegada]}, #{params[:comuna_llegada]}"
+    params.delete(:calle_llegada)
+    params.delete(:numero_llegada)
+    params.delete(:comuna_llegada)
+
+    params
+  end
 end
